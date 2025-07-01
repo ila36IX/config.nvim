5018b9ce8e5e0598568b2c47b1289dad3867e195
ila36IX <jbariali002@gmail.com>
2025-07-01 02:44:26 +0100

    Add Aligndelim command

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -77,3 +77,43 @@ end, { desc = 'Kill process' })
 
 -- Run the make program and fill Quickfix with errors if found
 vim.keymap.set({ 'n' }, '<leader>m', ':make<CR>', { desc = 'Kill process' })
+
+vim.api.nvim_create_user_command('AlignDelim', function()
+  -- Prompt for delimiter
+  local delim = vim.fn.input 'delim: '
+  if delim == '' then
+    return
+  end
+
+  -- Get visual range
+  local start_line = vim.fn.getpos("'<")[2]
+  local end_line = vim.fn.getpos("'>")[2]
+  local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+
+  -- Find max delimiter position
+  local max_col = 0
+  for _, line in ipairs(lines) do
+    local col = string.find(line, delim, 1, true)
+    while col and col > 2 and line:sub(col - 1, col - 1) == ' ' do
+      col = col - 1
+    end
+    if col and col > max_col then
+      max_col = col + 1
+    end
+  end
+  -- Align using the must far away delim
+  for i, line in ipairs(lines) do
+    local start, _end = string.find(line, delim, 1, true)
+    while start and start > 2 and line:sub(start - 1, start - 1) == ' ' do
+      start = start - 1
+    end
+    if start and _end then
+      lines[i] = line:sub(0, start - 1) .. string.rep(' ', max_col - start) .. delim .. line:sub(_end + 1)
+    end
+  end
+
+  -- Replace lines in buffer
+  vim.api.nvim_buf_set_lines(0, start_line - 1, end_line, false, lines)
+end, { range = true })
+
+vim.keymap.set({ 'v' }, '<leader>a', ":'<,'>AlignDelim<CR>", { desc = 'AlignDelim' })
